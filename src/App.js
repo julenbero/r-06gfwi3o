6627ddd2a1db98b68876217bc2a825ec90e8a629
Julio Bermudez <julenbero@gmail.com>
2020-11-24 20:56:00 -0500

    Exercise Complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import Welcome from './Welcome';
 
 class App extends Component {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.state = {
-      names: ["Juan", "Pedro", "Germán"]
-    }
-  }
+		this.state = {
+			names : [ 'Juan', 'Pedro', 'Germán' ]
+		};
+	}
 
-  render() {
-    return (
-      <div>
-        {this.state.names.map((name) =>
-          <Welcome name={name} />
-        )}
-      </div>
-    );
-  }
+	render() {
+		return <div>{this.state.names.map((name, i) => <Welcome key={i} name={name} />)}</div>;
+	}
 }
 
 export default App;
